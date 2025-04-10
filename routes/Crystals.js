346dd08b3ba6eae38b0c4c96d12793ce99daaa87
CharanReddy2504 <chilipiredd.ycharanreddy@gmail.com>
2025-04-09 20:10:30 -0500

    updated

--- routes/Crystals.js
+++ routes/Crystals.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Crystals', { title: 'Search Results:Crystals' });
-});
+// Import the controller for crystal
+var crystal_controller = require('../controllers/crystal');
+
+// CRUD operations for Crystal
+// GET request for list of all crystals
+router.get('/crystals', crystal_controller.crystal_list); 
+
+// GET request for a specific crystal by ID
+router.get('/crystals/:id', crystal_controller.crystal_detail);
+
+// POST request to create a new crystal
+router.post('/crystals', crystal_controller.crystal_create_post);
+
+// PUT request to update a crystal
+router.put('/crystals/:id', crystal_controller.crystal_update_put);
+
+// DELETE request to delete a crystal
+router.delete('/crystals/:id', crystal_controller.crystal_delete);
 
 module.exports = router;
